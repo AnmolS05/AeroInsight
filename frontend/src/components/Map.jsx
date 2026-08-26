4d991ae408296892efaacc240b2d7dd9021e5c91
AnmolS05 <anmolspoojary@gmail.com>
2026-08-26 22:46:24 +0530

    feat(map): Add support for VITE_CARTO_API_KEY environment variable

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -109,8 +109,8 @@ export default function Map({ telemetryData }) {
       >
       <ZoomControl position="bottomright" />
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url={`https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png${import.meta.env.VITE_CARTO_API_KEY ? '?api_key=' + import.meta.env.VITE_CARTO_API_KEY : ''}`}
       />
       <Polyline 
         positions={positions} 
